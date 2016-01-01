b06fe622176f1aa0b234aa7bb0ae35dc376bcaaf
Nina Kin <matikin9@gmail.com>
2015-12-31 17:09:55 -0800

    Refactor

--- script/script.js
+++ script/script.js
@@ -1,63 +1,63 @@
 function display_colors_needed(filenameArr) {
-	var numFiles = filenameArr.length;
-	var colorsNeeded = [];
+  var numFiles = filenameArr.length;
+  var colorsNeeded = [];
 
-	if (numFiles <= 1) {
-		return;
-	} else {
-		var stash = filenameArr[0];
-		var patterns = filenameArr.slice(1);
+  if (numFiles <= 1) {
+    return;
+  } else {
+    var stash = filenameArr[0];
+    var patterns = filenameArr.slice(1);
 
-		$.getJSON(stash, function(stashData) {
-			$.each(patterns, function(index, filename) {
-				$.getJSON(filename, function(patternData) {
+    $.getJSON(stash, function(stashData) {
+      $.each(patterns, function(index, filename) {
+        $.getJSON(filename, function(patternData) {
 
-					var colorsNeededInFile = get_colors_needed(stashData.colors, patternData.colors);
+          var colorsNeededInFile = get_colors_needed(stashData.colors, patternData.colors);
 
-					$.each(colorsNeededInFile, function(index, value) {
-						if ($.inArray(value, colorsNeeded) < 0) {
-							colorsNeeded.push(value);
-						}
-					});
+          $.each(colorsNeededInFile, function(index, value) {
+            if ($.inArray(value, colorsNeeded) < 0) {
+              colorsNeeded.push(value);
+            }
+          });
 
-					var html = "";
-					$.each(colorsNeeded, function(key, value) {
-						html += '<li>' + value + '</li>';
-					});
+          var html = "";
+          $.each(colorsNeeded, function(key, value) {
+            html += '<li>' + value + '</li>';
+          });
 
-					$('ul#neededColors').html(html);
-				});
-			});
-			return;
-		});
-}
+          $('ul#neededColors').html(html);
+        });
+      });
+      return;
+    });
+  }
 }
 
 function get_colors_needed(colorsOwned, colorsInPattern) {
-	// Takes in arrays of strings
-	if (colorsOwned.length === 0) {
-		return colorsInPattern;
-	} else if (colorsInPattern.length === 0) {
-		return [];
-	} else {
-		var colorsNeeded = [];
+  // Takes in arrays of strings
+  if (colorsOwned.length === 0) {
+    return colorsInPattern;
+  } else if (colorsInPattern.length === 0) {
+    return [];
+  } else {
+    var colorsNeeded = [];
 
-		$.each(colorsInPattern, function(index, obj) {
-			// if a color within colorset2 does not exist within uniqueElements (which was
-			// initialized to colorset1), add it to uniqueElements
-			if ($.inArray(obj, colorsOwned) < 0) {
-				colorsNeeded.push(obj);
-			}
-		});
+    $.each(colorsInPattern, function(index, obj) {
+      // if a color within colorset2 does not exist within uniqueElements (which was
+      // initialized to colorset1), add it to uniqueElements
+      if ($.inArray(obj, colorsOwned) < 0) {
+        colorsNeeded.push(obj);
+      }
+    });
 
-		return colorsNeeded;
-	}
+    return colorsNeeded;
+  }
 }
 
 function display_unique_colors(filenameArr) {
   // filenameArr: ["set1.json", "set2.json", ...]
-	// TODO: Check for duplicate colors within a single dataset
-	// TODO: Pull out display code to utilize callback functions
+  // TODO: Check for duplicate colors within a single dataset
+  // TODO: Pull out display code to utilize callback functions
 
   var numFiles = filenameArr.length;
   var uniqueColors = [];
@@ -72,42 +72,42 @@ function display_unique_colors(filenameArr) {
     $.each(filenameArr, function(index, filename) {
       // for each file in the array, compare elements to uniqueColors array
       $.getJSON(filename, function(data) {
-				var colorsToCompare = uniqueColors.slice(0);
+        var colorsToCompare = uniqueColors.slice(0);
         uniqueColors = get_unique_colors(colorsToCompare, data.colors);
-				uniqueColors.sort();
+        uniqueColors.sort();
 
-				var html = "";
-				$.each(uniqueColors, function(key, value) {
-					html += '<li>' + value + '</li>';
-				});
+        var html = "";
+        $.each(uniqueColors, function(key, value) {
+          html += '<li>' + value + '</li>';
+        });
 
-				$('ul#uniqueColors').html(html);
+        $('ul#uniqueColors').html(html);
       });
     });
-		return;
+    return;
   }
 }
 
 function get_unique_colors(colorset1, colorset2) {
-	// Takes in arrays of strings.
+  // Takes in arrays of strings.
   if (colorset1.length === 0) {
     return colorset2;
   } else if (colorset2.length === 0) {
     return colorset1;
   } else {
-		// Javascript passes arrays and objects by reference, not value.
-		// Using slice here sets uniqueElements to a shallow copy of
-		// the values in the colors1 array.
+    // Javascript passes arrays and objects by reference, not value.
+    // Using slice here sets uniqueElements to a shallow copy of
+    // the values in the colors1 array.
     var uniqueElements = colorset1.slice(0);
 
     $.each(colorset2, function(index, obj) {
-			// if a color within colorset2 does not exist within uniqueElements (which was
-			// initialized to colorset1), add it to uniqueElements
-			if ($.inArray(obj, uniqueElements) < 0) {
-				uniqueElements.push(obj);
-			}
+      // if a color within colorset2 does not exist within uniqueElements (which was
+      // initialized to colorset1), add it to uniqueElements
+      if ($.inArray(obj, uniqueElements) < 0) {
+        uniqueElements.push(obj);
+      }
     });
 
-		return uniqueElements;
+    return uniqueElements;
   }
 }
